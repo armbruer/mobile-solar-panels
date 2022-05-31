fa60fa2f556ed752ae758c6776e20ec2945c1dda
Florian Freund <florian88freund@gmail.com>
2022-05-31 13:27:24 +0200

    #1 Implement temperature sensor

--- iot-esp/src/main.rs
+++ iot-esp/src/main.rs
@@ -1,9 +1,27 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+mod sensors;
 
-fn main() {
-    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
-    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+use esp_idf_hal::prelude::Peripherals;
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_sys::EspError;
+
+use sensors::temperature::TemperatureSensor;
+
+fn main() -> Result<(), EspError> {
     esp_idf_sys::link_patches();
 
-    println!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let pins = peripherals.pins;
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+    let mut temp_sensor = TemperatureSensor::new(peripherals.i2c0, pins.gpio21, pins.gpio22)?;
+
+    loop {
+        log::info!(
+            "Temperature sensor reading: {}°C",
+            temp_sensor.get_temperature().unwrap()
+        );
+
+        std::thread::sleep(std::time::Duration::from_secs(2));
+    }
 }
---
+++ iot-esp/src/sensors/temperature.rs
@@ -0,0 +1,40 @@
+use embedded_drivers::bmp180::BMP180;
+use esp_idf_hal::{
+    gpio::{InputPin, OutputPin},
+    i2c,
+    i2c::{I2c, Master},
+    prelude::KiloHertz,
+};
+use esp_idf_sys::EspError;
+
+pub struct TemperatureSensor<I2C: I2c, SDA: OutputPin + InputPin, SCL: OutputPin> {
+    bmp180: BMP180<Master<I2C, SDA, SCL>>,
+}
+
+impl<I2C: I2c, SDA: OutputPin + InputPin, SCL: OutputPin> TemperatureSensor<I2C, SDA, SCL> {
+    pub fn new(
+        i2c: I2C,
+        i2c_pin_sda: SDA,
+        i2c_pin_scl: SCL,
+    ) -> Result<TemperatureSensor<I2C, SDA, SCL>, EspError> {
+        let config =
+            <i2c::config::MasterConfig as Default>::default().baudrate(KiloHertz::from(400).into());
+        let i2c_master = i2c::Master::new(
+            i2c,
+            i2c::MasterPins {
+                sda: i2c_pin_sda,
+                scl: i2c_pin_scl,
+            },
+            config,
+        )?;
+
+        let bmp180 = embedded_drivers::bmp180::BMP180::new(i2c_master);
+
+        Ok(TemperatureSensor { bmp180 })
+    }
+
+    pub fn get_temperature(&mut self) -> Result<f32, EspError> {
+        let temperature = self.bmp180.get_temperature(&mut esp_idf_hal::delay::Ets);
+        Ok(temperature)
+    }
+}
