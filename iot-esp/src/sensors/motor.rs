b7b9bae04071f22a8558c29d388afbde17b3ccfb
Florian Freund <florian88freund@gmail.com>
2022-06-02 02:28:04 +0200

    ESP: Improve stepper motor implementation

--- iot-esp/src/sensors/motor.rs
+++ iot-esp/src/sensors/motor.rs
@@ -1,15 +1,21 @@
 use embedded_hal::digital::v2::PinState;
-//use esp_idf_hal::gpio::OutputPin;
-use esp_idf_sys::EspError;
 
 use std::thread;
 use std::time::Duration;
 
+#[derive(Clone, Copy, Debug)]
+pub enum Speed {
+    // max: 16000
+    LowSpeed = 10000,
+    HighSpeed = 1000,
+    __Stop = 0, // Internal only
+}
+
 pub struct StepperMotor<
-    OUTPUT_PIN1: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
-    OUTPUT_PIN2: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
-    OUTPUT_PIN3: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
-    OUTPUT_PIN4: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
+    OUTPUT_PIN1: embedded_hal::digital::v2::OutputPin,
+    OUTPUT_PIN2: embedded_hal::digital::v2::OutputPin,
+    OUTPUT_PIN3: embedded_hal::digital::v2::OutputPin,
+    OUTPUT_PIN4: embedded_hal::digital::v2::OutputPin,
 > {
     pin1: OUTPUT_PIN1,
     pin2: OUTPUT_PIN2,
@@ -18,10 +24,10 @@ pub struct StepperMotor<
 }
 
 impl<
-        OUTPUT_PIN1: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
-        OUTPUT_PIN2: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
-        OUTPUT_PIN3: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
-        OUTPUT_PIN4: esp_idf_hal::gpio::OutputPin + embedded_hal::digital::v2::OutputPin,
+        OUTPUT_PIN1: embedded_hal::digital::v2::OutputPin,
+        OUTPUT_PIN2: embedded_hal::digital::v2::OutputPin,
+        OUTPUT_PIN3: embedded_hal::digital::v2::OutputPin,
+        OUTPUT_PIN4: embedded_hal::digital::v2::OutputPin,
     > StepperMotor<OUTPUT_PIN1, OUTPUT_PIN2, OUTPUT_PIN3, OUTPUT_PIN4>
 {
     pub fn new(
@@ -29,16 +35,16 @@ impl<
         pin2: OUTPUT_PIN2,
         pin3: OUTPUT_PIN3,
         pin4: OUTPUT_PIN4,
-    ) -> Result<StepperMotor<OUTPUT_PIN1, OUTPUT_PIN2, OUTPUT_PIN3, OUTPUT_PIN4>, EspError> {
-        Ok(StepperMotor {
+    ) -> StepperMotor<OUTPUT_PIN1, OUTPUT_PIN2, OUTPUT_PIN3, OUTPUT_PIN4> {
+        StepperMotor {
             pin1,
             pin2,
             pin3,
             pin4,
-        })
+        }
     }
 
-    pub fn rotateRight(&mut self, motorSpeed: u32) {
+    pub fn rotateRight(&mut self, motorSpeed: Speed) {
         self.setMotor(
             PinState::Low,
             PinState::Low,
@@ -97,7 +103,7 @@ impl<
         );
     }
 
-    pub fn rotateLeft(&mut self, motorSpeed: u32) {
+    pub fn rotateLeft(&mut self, motorSpeed: Speed) {
         self.setMotor(
             PinState::High,
             PinState::Low,
@@ -162,7 +168,7 @@ impl<
             PinState::Low,
             PinState::Low,
             PinState::Low,
-            0,
+            Speed::__Stop,
         );
     }
 
@@ -172,7 +178,7 @@ impl<
         in2: PinState,
         in3: PinState,
         in4: PinState,
-        motorSpeed: u32,
+        motorSpeed: Speed,
     ) {
         self.pin1.set_state(in1);
         self.pin2.set_state(in2);
