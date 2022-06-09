9d209454abbc68f29c167eb649cc060f29b6471c
Florian Freund <florian88freund@gmail.com>
2022-06-09 17:55:45 +0200

    ESP: Restructure code that all demo applications can be activated

--- iot-esp/src/main.rs
+++ iot-esp/src/main.rs
@@ -7,6 +7,7 @@ use std::time::Duration;
 use adc_interpolator::AdcInterpolator;
 use coap_lite::RequestType;
 use esp_idf_hal::adc;
+use esp_idf_hal::gpio::{Gpio34, Gpio35};
 use esp_idf_hal::prelude::Peripherals;
 
 use esp_idf_svc::netif::EspNetifStack;
@@ -15,9 +16,9 @@ use esp_idf_svc::sysloop::EspSysLoopStack;
 use esp_idf_sys::EspError;
 use esp_idf_sys::{self as _, sleep}; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
+use networking::coap::Connection;
 use sensors::motor::StepperMotor;
 use sensors::temperature::TemperatureSensor;
-use networking::coap::Connection;
 
 fn main() -> Result<(), EspError> {
     esp_idf_sys::link_patches();
@@ -64,97 +65,116 @@ fn main() -> Result<(), EspError> {
             (3500, 3500),
         ],
     };
-    /*
 
     let pin_photoresistor = pins.gpio34.into_analog_atten_11db()?;
     let pin_ir_sensor = pins.gpio35.into_analog_atten_11db()?;
-    let mut interpolator_photoresistor =
+    let mut interpolator_photoresistor: AdcInterpolator<Gpio34<adc::Atten11dB<adc::ADC1>>, u16, 3> =
         AdcInterpolator::new(pin_photoresistor, config_photoresistor);
-    let mut interpolator_ir_sensor = AdcInterpolator::new(pin_ir_sensor, config_ir_sensor);
+    let mut interpolator_ir_sensor: AdcInterpolator<Gpio35<adc::Atten11dB<adc::ADC1>>, u16, 3> =
+        AdcInterpolator::new(pin_ir_sensor, config_ir_sensor);
 
     let mut powered_adc = adc::PoweredAdc::new(
         peripherals.adc1,
         adc::config::Config::new().calibration(true),
     )?;
 
-    */
-
     let netif_stack = Arc::new(EspNetifStack::new()?);
     let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
     let default_nvs = Arc::new(EspDefaultNvs::new()?);
 
-    // TODO hostname
-    let _wifi = networking::wifi::wifi(
-        netif_stack.clone(),
-        sys_loop_stack.clone(),
-        default_nvs.clone(),
-    )?;
-
-    /* 
-    let thread_stepper_motor1 = std::thread::spawn(move || {
-        for _ in 0..20 {
-            for _ in 0..200 {
-                stepper_motor1.rotateRight(sensors::motor::Speed::HighSpeed);
+    // Demo: Hardware measurements on serial port and motors turning
+    let demo_hardware_measurements = false;
+    if demo_hardware_measurements {
+        let thread_stepper_motor1 = std::thread::spawn(move || {
+            for _ in 0..20 {
+                for _ in 0..200 {
+                    stepper_motor1.rotateRight(sensors::motor::Speed::HighSpeed);
+                }
+                stepper_motor1.stopMotor();
+                std::thread::sleep(std::time::Duration::from_secs(1));
             }
-            stepper_motor1.stopMotor();
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        }
-    });
-    let thread_stepper_motor2 = std::thread::spawn(move || {
-        for _ in 0..20 {
-            for _ in 0..200 {
-                stepper_motor2.rotateLeft(sensors::motor::Speed::HighSpeed);
+        });
+        let thread_stepper_motor2 = std::thread::spawn(move || {
+            for _ in 0..20 {
+                for _ in 0..200 {
+                    stepper_motor2.rotateLeft(sensors::motor::Speed::HighSpeed);
+                }
+                stepper_motor2.stopMotor();
+                std::thread::sleep(std::time::Duration::from_secs(1));
             }
-            stepper_motor2.stopMotor();
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        }
-    });
+        });
 
-    let thread_measure = std::thread::spawn(move || loop {
-        let photoresistor = interpolator_photoresistor.read(&mut powered_adc).unwrap();
-        let ir_sensor = interpolator_ir_sensor.read(&mut powered_adc).unwrap();
+        let thread_measure = std::thread::spawn(move || loop {
+            let photoresistor = interpolator_photoresistor.read(&mut powered_adc).unwrap();
+            let ir_sensor = interpolator_ir_sensor.read(&mut powered_adc).unwrap();
 
-        log::info!(
-            "Photoresitor: {:#?}, IR Sensor: {:#?}",
-            photoresistor,
-            ir_sensor
-        );
+            log::info!(
+                "Photoresitor: {:#?}, IR Sensor: {:#?}",
+                photoresistor,
+                ir_sensor
+            );
 
-        log::info!(
-            "Temperature: {} °C, Pressure: {} Pa",
-            temp_sensor.get_temperature(),
-            temp_sensor.get_pressure()
-        );
+            log::info!(
+                "Temperature: {} °C, Pressure: {} Pa",
+                temp_sensor.get_temperature(),
+                temp_sensor.get_pressure()
+            );
 
-        std::thread::sleep(std::time::Duration::from_secs(2));
-    });
+            std::thread::sleep(std::time::Duration::from_secs(2));
+        });
 
-    thread_stepper_motor1.join().unwrap();
-    thread_stepper_motor2.join().unwrap();
-    thread_measure.join().unwrap();
-
-    */
-    let mut conn = Connection::new();
-
-    loop {
-        send_sensor_data(&mut conn, "10.42.0.1:5683", &vec![1.0, 2.0, 3.0], &vec![4, 5, 6], &vec![7, 8, 9]);
-        log::info!("Sent a message");
-        std::thread::sleep(Duration::from_secs(2));
-
+        thread_stepper_motor1.join().unwrap();
+        thread_stepper_motor2.join().unwrap();
+        thread_measure.join().unwrap();
     }
+
+    let demo_coap = false;
+    if demo_coap {
+        // TODO hostname
+        let _wifi = networking::wifi::wifi(
+            netif_stack.clone(),
+            sys_loop_stack.clone(),
+            default_nvs.clone(),
+        )?;
+
+        let mut conn = Connection::new();
+
+        loop {
+            send_sensor_data(
+                &mut conn,
+                "10.42.0.1:5683",
+                &vec![1.0, 2.0, 3.0],
+                &vec![4, 5, 6],
+                &vec![7, 8, 9],
+            );
+            log::info!("Sent a message");
+            std::thread::sleep(Duration::from_secs(2));
+        }
+    }
+
+    Ok(())
 }
 
-
-fn send_sensor_data(conn: &mut Connection, addr: &str, temperature: &[f32], photoresistor: &[i32], infrared: &[i32]) {
+fn send_sensor_data(
+    conn: &mut Connection,
+    addr: &str,
+    temperature: &[f32],
+    photoresistor: &[i32],
+    infrared: &[i32],
+) {
     let mut payload = vec![];
 
     debug_assert_eq!(temperature.len(), photoresistor.len());
     debug_assert_eq!(photoresistor.len(), infrared.len());
 
     payload.extend_from_slice(&(temperature.len() as u32).to_ne_bytes());
-    
+
     // TODO prevent fragementation
-    for ((t,p),i) in temperature.iter().zip(photoresistor.iter()).zip(infrared.iter()) {
+    for ((t, p), i) in temperature
+        .iter()
+        .zip(photoresistor.iter())
+        .zip(infrared.iter())
+    {
         payload.extend_from_slice(&t.to_le_bytes());
         payload.extend_from_slice(&p.to_le_bytes());
         payload.extend_from_slice(&i.to_le_bytes());
