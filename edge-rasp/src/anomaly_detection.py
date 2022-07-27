c8ac102f79aa46d232f136cd6c7b8da2ebe3f763
Eric Armbruster <eric1@armbruster-online.de>
2022-07-27 10:18:38 +0200

    Edge: Move mail code to own file

--- edge-rasp/src/anomaly_detection.py
+++ edge-rasp/src/anomaly_detection.py
@@ -6,11 +6,10 @@ import db
 import asyncpg
 import pandas as pd
 import datetime
-import aiosmtplib
+import mail
 
 from sklearn.cluster import DBSCAN
 from model import Config
-from email.message import EmailMessage
 
 MIN_DATAPOINTS = 20
 ANOMALY_DETECTION_INTERVAL = int(datetime.timedelta(
@@ -34,47 +33,7 @@ async def worker(pool: asyncpg.Pool, conf: Config):
             logging.warning("Not enough data for anomaly detection available")
             continue
 
-        await send_mail(conf, await run_dbscan(df))
-
-
-async def send_mail(conf: Config, outliers_df: pd.DataFrame):
-    device_ids = outliers_df['device_id'].unique()
-    device_ids.sort()
-    devices = ', '.join(map(str, device_ids))
-
-    outliers_dfs = [y for _, y in outliers_df.groupby('device_id', as_index=False)]
-    anomalies = []
-    for df in outliers_dfs:
-        device_id = str(df.head(1)['device_id'].values[0])
-        anomaly_datetimes = ', '.join(map(lambda x: x.isoformat(),
-                                          df['time'].sort_values()))
-        anomalies.append(device_id + ': ' + anomaly_datetimes)
-
-    outliers = '\n'.join(anomalies)
-
-    content = f"""Hello,
-we have detected anomalies in your solar plants. The following devices may be affected:
-
-{devices}
-
-{outliers}
-
-Best regards,
-Your Mobile Solar Panels Team
-"""
-
-    message = EmailMessage()
-    message["From"] = conf.anomaly_detection.smtp.user
-    message["Subject"] = "Report: Anomalies detected"
-    message.set_content(content)
-
-    for email_receiver in conf.anomaly_detection.email_receivers:
-        message["To"] = email_receiver
-
-        await aiosmtplib.send(message=message, sender=conf.anomaly_detection.smtp.email_sender,
-                              recipients=email_receiver, hostname=conf.anomaly_detection.smtp.host,
-                              port=conf.anomaly_detection.smtp.port, username=conf.anomaly_detection.smtp.user,
-                              password=conf.anomaly_detection.smtp.password, use_tls=True)
+        await mail.send_mail(conf, await run_dbscan(df))
 
 
 async def run_dbscan(df: pd.DataFrame):
---
+++ edge-rasp/src/mail.py
@@ -0,0 +1,45 @@
+import aiosmtplib
+import pandas as pd
+
+from email.message import EmailMessage
+from model import Config
+
+
+async def send_mail(conf: Config, outliers_df: pd.DataFrame):
+    device_ids = outliers_df['device_id'].unique()
+    device_ids.sort()
+    devices = ', '.join(map(str, device_ids))
+
+    outliers_dfs = [y for _, y in outliers_df.groupby('device_id', as_index=False)]
+    anomalies = []
+    for df in outliers_dfs:
+        device_id = str(df.head(1)['device_id'].values[0])
+        anomaly_datetimes = ', '.join(map(lambda x: x.isoformat(),
+                                          df['time'].sort_values()))
+        anomalies.append(device_id + ': ' + anomaly_datetimes)
+
+    outliers = '\n'.join(anomalies)
+
+    content = f"""Hello,
+we have detected anomalies in your solar plants. The following devices may be affected:
+
+{devices}
+
+{outliers}
+
+Best regards,
+Your Mobile Solar Panels Team
+"""
+
+    message = EmailMessage()
+    message["From"] = conf.anomaly_detection.smtp.user
+    message["Subject"] = "Report: Anomalies detected"
+    message.set_content(content)
+
+    for email_receiver in conf.anomaly_detection.email_receivers:
+        message["To"] = email_receiver
+
+        await aiosmtplib.send(message=message, sender=conf.anomaly_detection.smtp.email_sender,
+                              recipients=email_receiver, hostname=conf.anomaly_detection.smtp.host,
+                              port=conf.anomaly_detection.smtp.port, username=conf.anomaly_detection.smtp.user,
+                              password=conf.anomaly_detection.smtp.password, use_tls=True)
